e78fc9c4d1842fe84b9274cc7c189296bf14a0ee
zhangjp <zjp@xlbzone.com>
2017-06-22 23:56:34 +0800

    v 0.1.2 完成定时任务

--- app/views.py
+++ app/views.py
@@ -2,27 +2,57 @@
 
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from datetime import datetime, date
 
 app = Flask(__name__)
+
+
+def str_to_date(string=None):
+    if not isinstance(string, str):
+        return None
+    string = string.strip()
+    try:
+        res = datetime.strptime(string, '%Y.%m.%d')
+        return res
+    except ValueError:
+        try:
+            res = datetime.strptime(string, '%Y-%m-%d')
+            return res
+        except ValueError:
+            pass
+    return None
 
 
 @app.route(r'/', methods=['GET'])
 def active_companies():
     """统计一段时间内每天活跃企业数"""
     data = []
-    start_date = request.args.get('from')
-    end_date = request.args.get('to')
-
     # 参数处理
-    if not start_date or not end_date:
-
-    #
-    start = start_date
-    end = end_date
+    start = str_to_date(request.args.get('from'))
+    end = str_to_date(request.args.get('to'))
+    provinceCode = request.args.get('provincecode')
+    if not start and not end:
+        start_date = end_date = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+    elif start is None or end is None:
+        if start is None:
+            start_date = end_date = end
+        else:
+            start_date = end_date = start
+    else:
+        start_date = start
+        end_date = end
     # 查询数据库
     # spv1.activecompanies
+
     mongo_client = MongoClient(host='127.0.0.1', port=27017)
-    active_record = mongo_client.spv1.activecompanies.find({'date':{'$gt':start,'$lt':end}})
-    for eachday in active_record:
-        eachday.get('')
-    return jsonify({'code':'200', 'data':data}), 200
+    active_record = mongo_client.spv1.activecompanies.find({'date': {'$gte': start_date, '$lte': end_date}},
+                                                           {'date': True, 'activeCompanyIds': True, '_id': False})
+
+    # 数据重组
+
+
+    return jsonify({'code': '200', 'data': None}), 200
+
+
+if __name__ == '__main__':
+    print(str_to_date())
--- scripts/crontab.py
+++ scripts/crontab.py
@@ -2,7 +2,6 @@
 """定时统计任务"""
 from datetime import tzinfo, timedelta, datetime, date
 from pymongo import MongoClient
-from bson import ObjectId
 
 
 class UTC(tzinfo):
@@ -21,37 +20,40 @@ class UTC(tzinfo):
         return timedelta(hours=self._offset)
 
 
-def stats(year, month, day):
+def stats(stats_day):
     """统计一天的活跃维修企业数
-    :param stats_day Date  被统计日期
-
+    :param stats_day date 被统计日期
     """
-
-    now = datetime(year,month,day)
+    record = datetime(stats_day.year, stats_day.month, stats_day.day, hour=0, minute=0, second=0,tzinfo=UTC(0))
     localtz = UTC(8)
-    start_of_day = datetime(now.year, now.month, now.day, hour=0, minute=0, second=0, tzinfo=localtz)
-    end_of_day = datetime(now.year, now.month, now.day + 1, tzinfo=localtz)
-    # 获取所有当天的上传维修记录
+    start_of_day = datetime(stats_day.year, stats_day.month, stats_day.day, hour=0, minute=0, second=0, tzinfo=localtz)
+    end_of_day = start_of_day + timedelta(days=1)
+    # 获取所有当天的上传维修记录的维修企业Id
     mongo_client = MongoClient(host='127.0.0.1', port=27017)
     cursor = mongo_client.spv1.maintenaces.distinct('companyId', {'created': {'$gte': start_of_day, '$lt': end_of_day}
                                                                   })
     # 维修企业ID
     company_ids = [_id for _id in cursor]
+    company_info = []
+    for _id in company_ids:
+        company = mongo_client.spv1.companies.find_one({'_id': _id})
+        if company:
+            info = {
+                '_id': _id,
+                'provinceCode': company.get('provinceCode'),
+                'cityCode': company.get('cityCode'),
+                'countyCode': company.get('countyCode'),
+            }
+            company_info.append(info)
 
-    #
-
-    print(company_ids)
-    # for _id in cursor:
-    #     ID = mongo_client.spv1.companies.find_one({'_id': _id},{'ID':True,'_id':False})
-    #     print(ID, _id)
-
-
+    print('%s-%s-%s 共 %s 家企业上传了维修记录' % (stats_day.year, stats_day.month, stats_day.day, len(company_ids)))
     # 写入统计数据库
-    mongo_client.spv1.activecompanies.replace_one({'date': now, },
-                                                  {'date': now, 'activeCompanyId': company_ids}, upsert=True)
-    # mongo_client.spv1.activecompanies.insert_many({'date': stats_day, 'activeCompanyId': company_ids})
-
+    mongo_client.spv1.activecompanies.replace_one({'date': record},
+                                                  {'date': record, 'activeCompanyIds': company_info}, upsert=True)
 
 if __name__ == '__main__':
-    stats_day = date(2017, 6, 19)
-    stats(2017, 6, 19)
+    basedate = date(2017, 1, 1)
+    delta = timedelta(1)
+    for i in range(180):
+        stats(basedate)
+        basedate += delta
